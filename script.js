d9d6e22217fc54b56172fc37d40c04c47bb89297
Willian Liquido <willian@liqudo.com.br>
2022-03-11 17:23:06 -0300

    adicionando animacoes

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ const api = "https://api.adviceslip.com/advice";
 const advice = document.querySelector("#advice");
 const btn = document.querySelector("#btn");
 const idAdvice = document.querySelector("#idAdvice");
+const container = document.querySelector(".container");
 
 function getAdvice() {
+  btn.classList.add("rotate");
+  container.classList.add("smooth");
   fetch(api)
-    .then((response) => response.json())
+    .then((response) => {
+      return response.json();
+    })
     .then(function (data) {
       advice.innerHTML = data.slip.advice;
       idAdvice.innerHTML = data.slip.id;
+      btn.classList.remove("rotate");
+      container.classList.remove("smooth");
     });
+
+  console.log(container.clientWidth, container.clientHeight);
 }
 getAdvice();
 btn.addEventListener("click", getAdvice);
